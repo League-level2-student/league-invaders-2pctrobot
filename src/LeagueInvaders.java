60ba68913799d9097fc38d11e8e8a098ddd74f4d
Student <Student@172.29.32.73>
2025-07-09 16:47:02 -0700

    SPACE!

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,10 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	public static void main(String[] args) {
+		final int WIDTH = 500;
+		final int HEIGHT = 800;
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+	}
 }
