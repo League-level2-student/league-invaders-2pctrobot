f4512632a33da4c5d2fcf7825a420d3b50862f2f
Student <Student@172.29.32.78>
2025-07-16 16:45:22 -0700

    Bwoop

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,10 +1,29 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
-	public static void main(String[] args) {
-		final int WIDTH = 500;
-		final int HEIGHT = 800;
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
+	GamePanel panel;
+	JFrame frame;
+	final int WIDTH = 500;
+	final int HEIGHT = 800;
+	
+	public LeagueInvaders() {
+		frame = new JFrame();
+		panel = new GamePanel();
 	}
+
+	
+	public static void main(String[] args) {
+		
+		LeagueInvaders bwoop = new LeagueInvaders();
+		bwoop.setup();
+	}
+
+	private void setup() {
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
 }
