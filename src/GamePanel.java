4c45f3c30b0077996e041acb8232ee1041660abe
527841 <527841@SDUSD-MZ01PR8T.sdcs.local>
2025-08-13 16:45:54 -0700

    bwop

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,24 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Timer;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements ActionListener{
 	Font titleFont = new Font("Arial", Font.PLAIN, 48);
 	Font subFont = new Font("Arial", Font.PLAIN, 32);
+	Timer frameDraw;
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
+	public GamePanel(){
+		frameDraw = new Timer(1000/60,this);
+	}
+	
 	public void updateMenuState() {  
 		
 	}
@@ -23,9 +35,10 @@ public class GamePanel extends JPanel{
 		g.setColor(Color.YELLOW);
 		g.drawString("LEAGUE INVADERS", 20, 100);
 		g.setFont(subFont);
-		g.drawString("Press ENTER to start", 100, 400);
+		g.drawString("Press ENTER to start", 90, 400);
+		g.drawString("Press SPACE for instructions", 35, 600);
 	}
-	public void drawGameState(Graphics g) {  
+	public void drawGameState(Graphics g) { 
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, 500, 800);
 	}
@@ -37,10 +50,7 @@ public class GamePanel extends JPanel{
 	
 	public void paintComponent(Graphics g){
 		g.fillRect(10, 10, 100, 100);
-		final int MENU = 0;
-	    final int GAME = 1;
-	    final int END = 2;
-	    int currentState = MENU;
+		
 	    if(currentState == MENU){
 	        drawMenuState(g);
 	    }else if(currentState == GAME){
@@ -48,5 +58,16 @@ public class GamePanel extends JPanel{
 	    }else if(currentState == END){
 	        drawEndState(g);
 	    }
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Action");
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		}
 	}
 }
